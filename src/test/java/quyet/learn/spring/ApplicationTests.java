f1d6daf7fae8c519bd78fe08e51c54c169c31d31
dxquyet16 <quyetcoder16@gmail.com>
2024-12-07 22:09:24 +0700

    first commit

---
+++ src/test/java/quyet/learn/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package quyet.learn.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
