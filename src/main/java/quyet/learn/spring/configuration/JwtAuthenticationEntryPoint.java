630b17de01e2966149c6b9bf5ec7a20fba22013f
dxquyet16 <quyetcoder16@gmail.com>
2024-12-11 14:17:12 +0700

    done auth basic

---
+++ src/main/java/quyet/learn/spring/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package quyet.learn.spring.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import quyet.learn.spring.dto.response.ApiResponse;
+import quyet.learn.spring.exception.ErrorCode;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getErrorCode())
+                .data(errorCode.getErrorMsg())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
