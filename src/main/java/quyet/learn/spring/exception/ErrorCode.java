9fa6ad5cc413676b53d6fa8f981408c2ddf77ac5
dxquyet16 <quyetcoder16@gmail.com>
2024-12-12 15:13:36 +0700

    done validation advandce

--- src/main/java/quyet/learn/spring/exception/ErrorCode.java
+++ src/main/java/quyet/learn/spring/exception/ErrorCode.java
@@ -10,11 +10,11 @@ public enum ErrorCode {
     INVALID_KEY(1001, "Invalid message key!", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1002, "User Existed!", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1003, "User Not Existed!", HttpStatus.NOT_FOUND),
-    USERNAME_INVALID(1004, "Username Must be at least 3 characters!", HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(1005, "Password Must be at least 6 characters!", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1004, "Username Must be at least {min} characters!", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1005, "Password Must be at least {min} characters!", HttpStatus.BAD_REQUEST),
     UNAUTHENTICATED(1006, "Unauthenticated Error!",HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "you do not have permission!",HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Invalid date of birth!",HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1008, "your age must be at least {min}",HttpStatus.BAD_REQUEST),
     ;
     private int errorCode;
     private String errorMsg;
