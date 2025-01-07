149ce3af2eb26a9c7eabf3ee099ae048fe051bf1
dxquyet16 <quyetcoder16@gmail.com>
2025-01-07 13:59:56 +0700

    done isolation in unit test

--- src/main/java/quyet/learn/spring/configuration/ApplicationInitConfig.java
+++ src/main/java/quyet/learn/spring/configuration/ApplicationInitConfig.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import quyet.learn.spring.entity.Users;
@@ -23,6 +25,11 @@ public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty(
+            prefix = "spring",
+            value = "datasource.driverClassName",
+            havingValue = "com.mysql.cj.jdbc.Driver"
+    )
     ApplicationRunner applicationRunner(UserRespository userRespository) {
         return args -> {
             if (userRespository.findByUsername("admin").isEmpty()) {
